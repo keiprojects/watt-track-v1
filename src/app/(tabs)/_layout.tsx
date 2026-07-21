ade13606361044bf1ceeb5abc243e532117a5d40
keiprojects <keiprojects.dev@gmail.com>
2026-07-21 09:59:01 +0800

    Add hidden drawer routes

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -92,6 +92,8 @@ export default function TabsLayout() {
             ),
           }}
         />
+        <Tabs.Screen name="profile-settings" options={{ href: null }} />
+        <Tabs.Screen name="data" options={{ href: null }} />
       </Tabs>
       <WattTrackDrawer visible={profileDrawerVisible} onClose={() => setProfileDrawerVisible(false)} />
     </>
